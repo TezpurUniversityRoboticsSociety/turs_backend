22e3ffcd0644915106471a82556c546c35141da4
sourishsarmah <sourishsharma1024@gmail.com>
2018-03-27 01:55:27 +0530

    registration UI

--- home/static/js/MemberController.js
+++ home/static/js/MemberController.js
@@ -5,7 +5,7 @@ app.controller('MemberController', ["$scope", function($scope){
         {
             name: "Nayanmoni Kakoty",
             desig: "President",
-            img: "../img/NAYAN.jpeg",
+            img: "./img/NAYAN.jpeg",
             link: "http://agnigarh.tezu.ernet.in/~nkakoty/",
             mail: "mailto:nkakoty@tezu.ernet.in",
             cardClass: "zeror-left animated"
@@ -13,7 +13,7 @@ app.controller('MemberController', ["$scope", function($scope){
         {
             name: "Dr. Riku Chutia",
             desig: "Vice President",
-            img: "../img/RIKU.jpeg",
+            img: "./img/RIKU.jpeg",
             link: "http://agnigarh.tezu.ernet.in/~riku/",
             mail: "mailto:riku@tezu.ernet.in",
             cardClass: "zeror-middle animated "
@@ -21,7 +21,7 @@ app.controller('MemberController', ["$scope", function($scope){
         {
             name: "Ms. Zahnupriya Kalita",
             desig: "Vice President",
-            img: "../img/ZK.jpeg",
+            img: "./img/ZK.jpeg",
             link: "http://www.tezu.ernet.in/dmech/people/zk.htm",
             mail: "malito:zk@tezu.ernet.in",
             cardClass: "zeror-middle animated"
@@ -29,7 +29,7 @@ app.controller('MemberController', ["$scope", function($scope){
         {
             name: "Mr. Zubeen Bhuyan",
             desig: "Technical Team Director",
-            img: "../img/ZB.jpeg",
+            img: "./img/ZB.jpeg",
             link: "http://agnigarh.tezu.ernet.in/~zubin/index.html",
             mail: "mailto:zubin@tezu.ernet.in",
             cardClass: "zeror-right animated"
@@ -43,7 +43,7 @@ app.controller('MemberController', ["$scope", function($scope){
         {
             name: "Rajat Sharma",
             desig: "Student Vice President",
-            img: "../img/RajatSharma.jpg",
+            img: "./img/RajatSharma.jpg",
             github: "https://github.com/rajatsharma369007",
             facebook: "https://www.facebook.com/profile.php?id=100002977450437",
             linkedin: "https://www.linkedin.com/in/rajat-sharma-1144a1119/",
@@ -61,7 +61,7 @@ app.controller('MemberController', ["$scope", function($scope){
         {
             name: "Dhruba Jyoti Sut",
             desig: "Joint Secretary",
-            img: "../img/DhrubaJyotiSut.jpeg",
+            img: "./img/DhrubaJyotiSut.jpeg",
             github: "",
             facebook: "https://www.facebook.com/dhrubajyoti.sut.3",
             linkedin: "",
@@ -70,7 +70,7 @@ app.controller('MemberController', ["$scope", function($scope){
         {
             name: "Devanga Borah",
             desig: "Treasurer",
-            img: "../img/DevangaBorah.jpeg",
+            img: "./img/DevangaBorah.jpeg",
             github: "",
             facebook: "",
             linkedin: "",
@@ -97,7 +97,7 @@ app.controller('MemberController', ["$scope", function($scope){
         {
             name: "Kanishka Chowdhury",
             desig: "Technical Team Member",
-            img: "../img/KanishkaChowdhury.jpeg",
+            img: "./img/KanishkaChowdhury.jpeg",
             github: "https://github.com/CKanishka",
             facebook: "https://www.facebook.com/kanishka.chowdhury.54",
             linkedin: "https://www.linkedin.com/in/kanishkachowdhury",
@@ -115,7 +115,7 @@ app.controller('MemberController', ["$scope", function($scope){
         {
             name: "Adil Bin Bhutto",
             desig: "Technical Team Member",
-            img: "../img/AdilBinBhutto.jpg",
+            img: "./img/AdilBinBhutto.jpg",
             github: "https://github.com/binbhutto",
             facebook: "https://www.facebook.com/adil4bhutto",
             linkedin: "https://www.linkedin.com/in/adil-b-686739b3/",
@@ -124,7 +124,7 @@ app.controller('MemberController', ["$scope", function($scope){
         {
             name: "Manoj Sharma",
             desig: "Technical Team Member",
-            img: "../img/ManojSharma.png",
+            img: "./img/ManojSharma.png",
             github: "https://github.com/manojsharma221",
             facebook: "https://www.facebook.com/ms9954098610",
             linkedin: "https://www.linkedin.com/in/manoj-sharma-6b59b9155/",
@@ -133,7 +133,7 @@ app.controller('MemberController', ["$scope", function($scope){
         {
             name: "Biswajit Bharali",
             desig: "Technical Team Member",
-            img: "../img/BiswajitBharali.jpeg",
+            img: "./img/BiswajitBharali.jpeg",
             github: "",
             facebook: "",
             linkedin: "",
@@ -142,7 +142,7 @@ app.controller('MemberController', ["$scope", function($scope){
         {
             name: "Saurabh Kannaujia",
             desig: "Technical Team Member",
-            img: "../img/SaurabhKannaujia.jpg",
+            img: "./img/SaurabhKannaujia.jpg",
             github: "https://github.com/imonstershadow",
             facebook: "https://www.facebook.com/saurabh.kannaujia.9",
             linkedin: "https://www.linkedin.com/in/saurabh-kannaujia-9b5418159",
@@ -151,7 +151,7 @@ app.controller('MemberController', ["$scope", function($scope){
         {
             name: "Prateek Saikia",
             desig: "Event Co-ordinating Team Member",
-            img: "../img/Prateeksaikia.jpeg",
+            img: "./img/Prateeksaikia.jpeg",
             github: "https://github.com/Prateekj7",
             facebook: "https://m.facebook.com/profile.php?id=100001788514377&ref=content_filter",
             linkedin: "",
@@ -160,7 +160,7 @@ app.controller('MemberController', ["$scope", function($scope){
         {
             name: "Sandeep Sarkar",
             desig: "Event Co-ordinating Team Member",
-            img: "../img/SANDEEPSARKAR.png",
+            img: "./img/SANDEEPSARKAR.png",
             github: "",
             facebook: "",
             linkedin: "",
@@ -169,7 +169,7 @@ app.controller('MemberController', ["$scope", function($scope){
         {
             name: "Deepankar Acharya",
             desig: "Event Co-ordinating Team Member",
-            img: "../img/DeepankarAcharya.jpeg",
+            img: "./img/DeepankarAcharya.jpeg",
             github: "",
             facebook: "",
             linkedin: "",
