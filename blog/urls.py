f386d5ea417c4137095b4f514f1721cc90394c70
sourishsarmah <sourishsharma1024@gmail.com>
2018-03-27 19:40:12 +0530

    Blog updated

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from django.views.generic import ListView,DetailView
 from blog.models import Post
 
 urlpatterns = [
-	path('', ListView.as_view(queryset=Post.objects.all().order_by("-date")[:25],template_name="blog/blog.html")),
-	url(r'(?P<pk>\d+)',DetailView.as_view(model=Post,template_name='blog/post.html'))
+	path('', ListView.as_view(queryset=Post.objects.all().order_by("-date")[:25],template_name="blog/BlogSite.html")),
+	url(r'(?P<pk>\d+)',DetailView.as_view(model=Post,template_name='blog/BlogSite.html'))
 ]
