6b5f6bccae6ee77fca2da4867c33bc3031ba4926
sourishsarmah <sourishsharma1024@gmail.com>
2018-03-28 00:11:46 +0530

    Updated

--- turobotics/settings.py
+++ turobotics/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'turobotics.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
