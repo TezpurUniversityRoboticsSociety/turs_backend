dd9790f1fe13c7657c61218deb8f1110f8fbd30f
sourishsarmah <sourishsharma1024@gmail.com>
2018-04-08 12:23:11 +0530

    Image size restriction added

--- home/views.py
+++ home/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from registration.models import Member
 
 def index(request):
-    executives = Member.objects.filter(registered=True).filter(membership='Executive Member')
-    return render(request,'home/home.html',{'executives':executives})
+    executive_prof = Member.objects.filter(registered=True).filter(membership='Executive Professor')
+    executive_stud = Member.objects.filter(registered=True).filter(membership='Executive Member')
+    return render(request,'home/home.html',{'executive_stud':executive_stud ,'executive_prof':executive_prof})
 
 def member(request):
     members = Member.objects.filter(registered=True).filter(membership='Member')
--- registration/models.py
+++ registration/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.conf import settings
-import os
+from django.core.exceptions import ValidationError
 
 # Create your models here.
 class Member(models.Model):
@@ -10,22 +10,30 @@ class Member(models.Model):
         ('Others', 'Others'),
     ]
     TYPE = [
+        ('Executive Professor', 'Executive Professor'),
         ('Executive Member', 'Executive Member'),
         ('Member', 'Member'),
         ('Alumni', 'Alumni'),
     ]
+
+    def validate_image(fieldfile_obj):
+        filesize = fieldfile_obj.file.size
+        megabyte_limit = 1.0
+        if filesize > megabyte_limit*1024*1024:
+            raise ValidationError("Max file size is %sMB" % str(megabyte_limit))
+    
+
     name = models.CharField(max_length=140,blank=False)
     roll = models.CharField(max_length=8,blank=True)
     gender = models.CharField(max_length=6,choices=SEX,default='Others')
-    course = models.CharField(max_length=20,blank=True)
+    course = models.CharField(max_length=25,blank=False)
     phone = models.IntegerField(blank=True,null=True)
     email = models.EmailField(max_length=100,blank=False)
-    photo = models.ImageField(default='default.png',blank=False, null=True)
-    id_card = models.ImageField(default='default.png',blank=True, null=True)
+    photo = models.ImageField(default='default.png',blank=False, null=True, validators=[validate_image])
     membership = models.CharField(max_length=20,choices=TYPE,default='Member')
-    designation = models.CharField(max_length=20,default='Member')
-    facebook = models.URLField(max_length=100,blank=True)
+    designation = models.CharField(max_length=30,default='Member')
     github = models.URLField(max_length=100,blank=True)
+    facebook = models.URLField(max_length=100,blank=True)
     linkedin = models.URLField(max_length=100,blank=True)
     date = models.DateTimeField(auto_now_add=True)
     registered = models.BooleanField(default=False)
