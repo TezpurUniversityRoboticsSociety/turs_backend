f71072c7ade597c79c006fc71aef4c479c645404
sourishsarmah <sourishsharma1024@gmail.com>
2018-04-08 21:23:37 +0530

    Timeline added to Database

--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Timeline(models.Model):
+    date = models.DateField(blank=False)
+    title = models.CharField(blank=False, max_length=30)
+    event = models.TextField(blank=False)
+
+    def __str__(self):
+        return self.title
--- home/views.py
+++ home/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from registration.models import Member
+from .models import Timeline
 
 def index(request):
+    events = Timeline.objects.all().order_by("date")[:10]
     executive_prof = Member.objects.filter(registered=True).filter(membership='Executive Professor')
     executive_stud = Member.objects.filter(registered=True).filter(membership='Executive Member')
-    return render(request,'home/home.html',{'executive_stud':executive_stud ,'executive_prof':executive_prof})
+    return render(request,'home/home.html',{'events':events, 'executive_stud':executive_stud, 'executive_prof':executive_prof})
 
 def member(request):
     members = Member.objects.filter(registered=True).filter(membership='Member')
