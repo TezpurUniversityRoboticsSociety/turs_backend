eae29b6fb110bf42827c46e60ae7674a453c485f
sourish <sourishsarmah@gmail.com>
2018-08-24 23:19:51 +0530

    Events changed to reversed chronologicl order

--- home/views.py
+++ home/views.py
@@ -3,7 +3,7 @@ from registration.models import Member
 from .models import Timeline
 
 def index(request):
-    events = Timeline.objects.all().order_by("-date")[:10][::-1]
+    events = Timeline.objects.all().order_by("date")[:10][::-1]
     executive_prof = Member.objects.filter(registered=True).filter(membership='Executive Professor')
     executive_stud = Member.objects.filter(registered=True).filter(membership='Executive Member')
     return render(request,'home/home.html',{'events':events, 'executive_stud':executive_stud, 'executive_prof':executive_prof})
